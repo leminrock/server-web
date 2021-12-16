55148cb6e51c31b793bc78b8e3033c318f084f8d
frbi-zvnt <francesco.bianchi.zvnt@outlook.com>
2021-12-17 00:10:15 +0100

    PDPATCH path updated

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    NOT_YET_IMPLEMENTED: { response_code: 'NOT_YET_IMPLEMENTED' },
-    NMCLI_NETWORKS_AVAILABLE: "nmcli -t -g in-use,ssid,chan,signal,security device wifi list",
-    PDPATCH_PATH: "/home/neum/Documenti/pdpatch"
-};
+    NOT_YET_IMPLEMENTED: { response_code: "NOT_YET_IMPLEMENTED" },
+    NMCLI_NETWORKS_AVAILABLE:
+        "nmcli -t -g in-use,ssid,chan,signal,security device wifi list",
+    PDPATCH_PATH: "/home/odisseo/Documenti/pdpatch",
+}
