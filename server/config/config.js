9e2493ecb57365120f67a2c5fb28a6e95e0397a4
franeum <elettrochuck@gmail.com>
2021-12-18 17:49:47 +0100

    FIX: set generic HOME path

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     NOT_YET_IMPLEMENTED: { response_code: "NOT_YET_IMPLEMENTED" },
-    NMCLI_NETWORKS_AVAILABLE:
-        "nmcli -t -g in-use,ssid,chan,signal,security device wifi list",
-    PDPATCH_PATH: "/home/odisseo/Documenti/pdpatch",
+    NMCLI_NETWORKS_AVAILABLE: "nmcli -t -g in-use,ssid,chan,signal,security device wifi list",
+    PDPATCH_PATH: `${process.env.HOME}/Documenti/pdpatch`,
 }
